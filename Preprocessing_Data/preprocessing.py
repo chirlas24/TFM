0d66a687111bcb5fd12c0aa0665823d0b8695f1f
Gonzalo Romero <45177328+chirlas24@users.noreply.github.com>
2019-05-03 16:00:16 +0200

    Add files via upload

--- Preprocessing_Data/preprocessing.py
+++ Preprocessing_Data/preprocessing.py
@@ -64,7 +64,7 @@ class GeoJSON(object):
 
 from datetime import datetime
 
-def date_process(date):
+def date_process_tracks(date):
     '''Convert date format of the data set into date_datetime format'''
     data_aux = date['$date'].split("T")
     time_aux = data_aux[1].split(":")
@@ -134,3 +134,119 @@ class station_status(object):
     def id_(self):
         '''Code for the station'''
         return self.state['id']
+				
+def make_dataset(df, value, verbose=False):
+    '''Make a times-series flat dataset with one type of data (value) of the dictionary.
+        i.e. date vs activation of the station'''
+    
+    df_aux = df.copy()
+    
+    for i, report_list_time in enumerate(df_aux['stations']):
+        
+        if verbose:
+            if i == 0:
+                verbose_fun(i+1, len(df_aux['stations']), first=True)
+            else:
+                verbose_fun(i+1, len(df_aux['stations']), first=False)
+
+        
+        for j, station in enumerate(df_aux['stations'][i]):
+            station_id = str(df['stations'][i][j]['id']) + '-' + df['stations'][i][j]['number']
+            df_aux[station_id + '_' + value] = df['stations'].map(lambda x : x[j][value])
+    
+    df_aux.drop('stations', axis=1, inplace=True)
+    
+    return df_aux
+
+def verbose_fun(part, total, first=True):
+    '''This function is complementary to long calculation functions
+        in order to visulize the process'''
+    if first: 
+        global _10
+        global _20
+        global _30
+        global _40
+        global _50
+        global _60
+        global _70
+        global _80
+        global _90
+        _10 = True
+        _20 = True
+        _30 = True
+        _40 = True
+        _50 = True
+        _60 = True
+        _70 = True
+        _80 = True
+        _90 = True
+    if part/total >= 0.1 and _10:
+        print("0%[--10%------------------]100%")
+        _10 = False
+    if part/total >= 0.2 and _20:
+        print("0%[----20%----------------]100%")
+        _20 = False
+    if part/total >= 0.3 and _30:
+        print("0%[------30%--------------]100%")
+        _30 = False
+    if part/total >= 0.4 and _40:
+        print("0%[--------40%------------]100%")
+        _40 = False
+    if part/total >= 0.5 and _50:
+        print("0%[----------50%----------]100%")
+        _50 = False
+    if part/total >= 0.6 and _60:
+        print("0%[------------60%--------]100%")
+        _60 = False
+    if part/total >= 0.7 and _70:
+        print("0%[--------------70%------]100%")
+        _70 = False
+    if part/total >= 0.8 and _80:
+        print("0%[----------------80%----]100%")
+        _80 = False
+    if part/total >= 0.9 and _90:
+        print("0%[------------------90%--]100%")
+        _90 = False
+    if part/total > 0.9 and not(_90):
+        print("DATAFRAME COMPLETED")
+				
+def date_process_stations(date):
+    '''Convert date format of the data set into date_datetime format'''
+    data_aux = date.split("T")
+    time_aux = data_aux[1].split(":")
+    date_list = [data_aux[0], time_aux[0], time_aux[1]]
+    date_string = ("-").join(date_list)
+    date_datetime = datetime.strptime(date_string, '%Y-%m-%d-%H-%M')
+    return date_datetime
+		
+def make_all_dataset(data_list, value, verbose=True):
+
+    df_all = pd.DataFrame()
+
+    for i, file in enumerate(data_list):
+
+        if verbose:
+                if i == 0:
+                    verbose_fun(i+1, len(data_list), first=True)
+                else:
+                    verbose_fun(i+1, len(data_list), first=False)
+
+        path_aux = os.path.join('DATA' ,file)
+        df_aux = pd.read_json(path_aux, lines=True)
+
+        df_aux['Date'] = df_aux['_id'].map(date_process_stations)
+        df_aux.drop('_id', inplace=True, axis=1)
+
+        df_aux = make_dataset(df_aux, value)
+
+        if df_all.empty:
+            df_all = df_aux
+        else:
+            df_all = pd.concat([df_all, df_aux])
+
+        if verbose:
+            print("=============================")
+            print(file, "added to DataSet")
+            print("=============================")
+    
+    return df_all
